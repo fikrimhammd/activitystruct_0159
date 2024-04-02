5ab21db8e04e5a7a2455292137f8f7df0e977ec1
fikrimhammd <fikri.kdi60@gmail.com>
2024-04-02 15:36:28 +0800

    include string

--- Pemdas.cpp
+++ Pemdas.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 
@@ -6,29 +7,24 @@ struct DetailAlamat {
 	string desa;
 	string kota;
 };
-
-
-
 struct mahasiswa {
 	string nim;
 	string nama;
 	DetailAlamat alamat;
 	int umur;
 };
-
 int main() {
-	mahasiswa  mhs;
-	cout << "nomor Mahasiswa : ";
-	cin >> mhs.nim;
-	cout << "Nama Mahasiswa :";
-	cin >> mhs.nama;
+	mahasiswa mhs;
+	cout << "Nomor mahasiswa : ";
+	getline(cin, mhs.nim);
+	cout << "Nama Mahasiswa : ";
+	getline(cin, mhs.nama);
 
-	cout << "alamat mahasiswa : ";
+	cout << "alamat mahasiswa : " << endl;
 	cout << "\t nama Desa : ";
 	cin >> mhs.alamat.desa;
 	cout << "\t Nama kota : ";
 	cin >> mhs.alamat.kota;
-
 	cout << "Umur mahasiswa : ";
 	cin >> mhs.umur;
 
@@ -36,8 +32,8 @@ int main() {
 	cout << "\n Nim :" << mhs.nim;
 	cout << "\n Nama : " << mhs.nama;
 	cout << "\n alamat : ";
-	cout << "\n Desa : " << mhs.alamat.desa;
-	cout << "\n Kota : " << mhs.alamat.kota;
+	cout << "\n \t Desa : " << mhs.alamat.desa;
+	cout << "\n \t Kota : " << mhs.alamat.kota;
 	cout << "\n Umur : " << mhs.umur;
 }
 
