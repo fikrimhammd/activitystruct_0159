a62277575dac239f2b450fc64772c8f84d83d2fe
fikrimhammd <fikri.kdi60@gmail.com>
2024-04-02 15:30:44 +0800

    struct DetailAlamat

--- Pemdas.cpp
+++ Pemdas.cpp
@@ -2,10 +2,17 @@
 using namespace std;
 
 
+struct DetailAlamat {
+	string desa;
+	string kota;
+};
+
+
+
 struct mahasiswa {
 	string nim;
 	string nama;
-	string alamat;
+	DetailAlamat alamat;
 	int umur;
 };
 
@@ -15,14 +22,23 @@ int main() {
 	cin >> mhs.nim;
 	cout << "Nama Mahasiswa :";
 	cin >> mhs.nama;
+
 	cout << "alamat mahasiswa : ";
-	cin >> mhs.alamat;
+	cout << "\t nama Desa : ";
+	cin >> mhs.alamat.desa;
+	cout << "\t Nama kota : ";
+	cin >> mhs.alamat.kota;
+
 	cout << "Umur mahasiswa : ";
 	cin >> mhs.umur;
 
 	cout << endl;
 	cout << "\n Nim :" << mhs.nim;
 	cout << "\n Nama : " << mhs.nama;
-	cout << "\n alamat : " << mhs.alamat;
+	cout << "\n alamat : ";
+	cout << "\n Desa : " << mhs.alamat.desa;
+	cout << "\n Kota : " << mhs.alamat.kota;
 	cout << "\n Umur : " << mhs.umur;
 }
+
+
